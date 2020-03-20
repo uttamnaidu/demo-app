0b6617fc63d6d51c449b8a547cbc8668754a52aa
uttam naidu <uttam.naidu@gmail.com>
2020-03-19 23:19:06 -0400

    Initial Checkin

---
+++ src/main/java/com/justiceleague/justiceleaguetracker/JusticeLeagueTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.justiceleague.justiceleaguetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusticeLeagueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JusticeLeagueTrackerApplication.class, args);
+    }
+}
